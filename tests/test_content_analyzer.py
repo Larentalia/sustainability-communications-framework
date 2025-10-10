61b3ced2facf7e7c7b0ae9d39988eb70888018fd
Begoña Penon García <150270001+Larentalia@users.noreply.github.com>
2025-10-10 21:15:02 +0200

    Delete tests directory

--- tests/test_content_analyzer.py
+++
@@ -1,9 +0,0 @@
-from content_analyzer.content_analyzer_code import ContentAnalyzer
-
-def test_content_analyzer_initialization():
-    # Creamos un ContentAnalyzer con segmentos de prueba
-    analyzer = ContentAnalyzer(["segment1", "segment2"])
-    
-    # Comprobamos que se inicializa correctamente
-    assert analyzer.content_segments == ["segment1", "segment2"]
-    assert analyzer.sustainability_elements == []
